e37a751ecc1b8a83fb13940363d5c0ce0088e5e3
Sean <mavangirasean@gmail.com>
2025-10-14 12:16:29 +0200

    Initial Commit

---
+++ Rydra/RydraApp.swift
@@ -0,0 +1,17 @@
+//
+//  RydraApp.swift
+//  Rydra
+//
+//  Created by Sean Mavangira on 14/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct RydraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
