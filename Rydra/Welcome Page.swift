50bf685568139734870864d0858a6a972eed668b
Sean <mavangirasean@gmail.com>
2025-10-15 19:12:28 +0200

    Created the welcome page. Added the App image, added welcome text, and created a button which takes us to the next page.

---
+++ Rydra/Welcome Page.swift
@@ -0,0 +1,68 @@
+import SwiftUI
+
+struct WelcomePage: View {
+    let fullText = "Welcome to Rydra"
+    @State private var displayedText = ""
+    @State private var animate = false
+
+    var body: some View {
+        NavigationStack{
+            VStack {
+                Image("Image 1")
+                    .resizable()
+                    .frame(width: 360, height: 300)
+                    .cornerRadius(20)
+                    .offset(y: -100)
+                
+                
+                Text(displayedText)
+                    .font(.largeTitle)
+                    .foregroundColor(.black)
+                    .bold()
+                    .shadow(color: .white.opacity(0.6), radius: 10)
+                    .multilineTextAlignment(.center)
+                    .opacity(animate ? 1 : 0.8)
+                    .scaleEffect(animate ? 1.02 : 1.0)
+                    .animation(.easeInOut(duration: 2).repeatForever(autoreverses: true), value: animate)
+                    .onAppear {
+                        animate = true
+                        typeText()
+                    }
+            }
+            NavigationLink{
+                Home()
+            }label: {
+                ZStack{
+                    
+                    RoundedRectangle(cornerRadius: 16)
+                        .frame(width: 150, height: 50)
+                    
+                        .foregroundStyle(.orange)
+                    Text("Next")
+                        .bold()
+                        .foregroundStyle(.white)
+                        .font(.title2)
+                }
+                
+            }
+            .offset(y: 100)
+        }
+        
+    }
+    private func typeText() {
+        displayedText = ""
+        var delay = 0.0
+        for letter in fullText {
+            DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
+                withAnimation(.easeInOut(duration: 0.2)) {
+                    displayedText.append(letter)
+                }
+            }
+            delay += 0.05
+        }
+    }
+}
+
+#Preview {
+   WelcomePage()
+}
