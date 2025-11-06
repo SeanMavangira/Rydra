04a2c85e9b49cf0867fd5d8f7c43f2ce4bb64287
Sean <mavangirasean@gmail.com>
2025-11-06 10:39:25 +0200

    Changed the backgrounds opacity

---
+++ Rydra/SettingsPage.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsPage.swift
+//  Rydra
+//
+//  Created by Sean Mavangira on 6/11/2025.
+//
+
+import SwiftUI
+
+struct SettingsPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsPage()
+}
